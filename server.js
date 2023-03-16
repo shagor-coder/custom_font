3a1363f829f0e8e1c1df9c5437e2da22107b6fd6
Md Shagor Hossain <79301601+shagor-coder@users.noreply.github.com>
2023-03-17 03:21:45 +0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+// Enable CORS
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+// Serve the custom font file
+app.get("/La_Beauties.woff2", function (req, res) {
+  res.sendFile(path.join(__dirname, "La_Beauties.woff2"));
+});
+
+// Start the server
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
